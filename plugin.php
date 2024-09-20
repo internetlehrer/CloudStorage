d1da74d283d4e9f13b922a23ec6d419ece81695b
Shared FHD Development <eqsoft4@gmail.com>
2024-09-20 10:48:51 +0200

    increase plugin version to 1.1

--- plugin.php
+++ plugin.php
@@ -4,8 +4,8 @@
 $id = "xcls";
 
 // code version; must be changed for all code changes
-$version = "1.0";
- 
+$version = "1.1";
+
 // ilias min and max version; must always reflect the versions that should
 // run with the plugin
 $ilias_min_version = "8.0";
